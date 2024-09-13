83537d58adcfbb1221151ba6ad949ba8af5379fc
sksayz5 <137992110+sksayz5@users.noreply.github.com>
2024-09-13 12:27:49 +0530

    Update blah.sh

--- blah.sh
+++ blah.sh
@@ -9,9 +9,11 @@ repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 15
 # Clone local_manifests repository
 git clone https://github.com/sksayz5/local_manifest.git --depth 1 -b main .repo/local_manifests
 
-# Sync the repositories
-/opt/crave/resync.sh
-
+if [ -f /opt/crave/resync.sh ]; then
+  /opt/crave/resync.sh
+else
+  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
 
 export BUILD_USERNAME=shravan
 export BUILD_HOSTNAME=crave
